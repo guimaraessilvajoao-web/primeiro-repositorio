eef2ddd1fb904c33ce89206477c060e530919005
guimaraessilvajoao-web <guimaraes.silva.joao@escola.pr.gov.br>
2026-08-12 21:12:15 -0300

    aula

---
+++ main.js
@@ -0,0 +1,18 @@
+const botoes = document.querySelectoraLL('button')
+
+botoes. forEach(function (botao)) {
+let curtiu = false;
+botao. addEventListener('click, botaoClicado');
+function botaoClicado(){
+console.log('fui clicado');
+let texto = botao.querySelrctor('span');
+if (curtiu ===false) {
+  texto.texContent++;
+  curtiu = true;  
+} else {
+texto.textCon
+
+}
+
+}
+    
