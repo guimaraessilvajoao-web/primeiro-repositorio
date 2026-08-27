f7592c3e7ead670629bdaf3fc2d8cf7574bb270e
guimaraessilvajoao-web <guimaraes.silva.joao@escola.pr.gov.br>
2026-08-26 21:15:22 -0300

    29

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const botoes = document.querySelectoraLL('button')
 
 botoes. forEach(function (botao)) {
-let curtiu = false;
-botao. addEventListener('click, botaoClicado');
-function botaoClicado(){
-console.log('fui clicado');
-let texto = botao.querySelrctor('span');
-if (curtiu ===false) {
-  texto.texContent++;
-  curtiu = true;  
+  let curtiu = false;
+  botao. addEventListener('click, botaoClicado');
+  function botaoClicado(){
+    console.log('fui clicado');
+    let texto = botao.querySelrctor('span');
+    if (curtiu ===false) {
+      texto.texContent++;
+      curtiu = true;  
 } else {
 texto.textCon
 
